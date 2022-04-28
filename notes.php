52886974b853dc5b4f9ab9f94bf7c51e31ef9e8e
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-04-28 21:36:30 +0530

    Update notes.php

--- notes.php
+++ notes.php
@@ -1,3 +1,13 @@
+MAIL_MAILER=smtp
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=465
+MAIL_USERNAME=pmbocerp@gmail.com
+MAIL_PASSWORD=svjqycklagioxgcq
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=pmbocerp@gmail.com
+MAIL_FROM_NAME="${APP_NAME}"
+
+
 Step 1:-
 First you migrate your migration.
 
